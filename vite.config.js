0915913813da2e7dd965ab99c5308a40058459c9
Phan Xuan Giang <springriver248@gmail.com>
2024-02-23 17:11:23 +0700

    config for build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     resolve: {
         alias: [{ find: '~', replacement: '/src' }],
+    },
+    build: {
+        manifest: 'manifest.json',
+        rollupOptions: {},
     },
 })
